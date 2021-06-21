8b7d7212d592ce6257692eee3e6dd740227326e2
Ben <classle55sc2@gmail.com>
2021-06-21 13:15:47 +0100

    minor styling changes

--- src/pages/userpage.js
+++ src/pages/userpage.js
@@ -7,6 +7,7 @@ import Footer from "./components/footer.js";
 // test api:
 export let data = require("../api/sample.json");
 
+// data handling: 
 let currentUser = data.users[0];
 let userName = currentUser.username;
 let userFullName = currentUser.userFullname;
@@ -16,15 +17,17 @@ let userBio = currentUser.userBio;
 console.log(currentUser);
 
 export class user extends Component {
+
     constructor(props) {
         super(props);
-        this.state = {
-            links: data.users[0].userLinks
+        this.state = { 
+            links: data.users[0].userLinks,
         };
       }
-  render() {
 
+  render() {
     let links = this.state.links;
+
     return (
       <div className={style.userPage}>
         {/* user page header */}
